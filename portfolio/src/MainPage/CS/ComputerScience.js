b946e45da7b7fd450d0dbc843ec0c19a3ea2cceb
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-04-06 14:26:35 -0400

    Updating my portfolio website to use React Router. Also creating separate sections for for my computer science and creative projects and experiences. So far all I have done is started creating the layout of how I want things. will need to do the same for creative, and a place to add general resume

---
+++ portfolio/src/MainPage/CS/ComputerScience.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css';
+
+function ComputerScience(){ {/* Projects, Resume, Github link, tech/languages i'm learning */}
+    return(
+        <>
+        <div>
+            {/* this is where I will have the projects bubble */}
+        </div>
+        <div>
+            {/* have a rotating carousel maybe of the technologies that I'm using */}
+        </div>
+        </>
+    )
+}
+
+export default ComputerScience
